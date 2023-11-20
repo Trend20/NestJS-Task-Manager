13b5809ac70e58874fdf8fe879e1e0fa371d0678
Trend20 <enockomondi305@gmail.com>
2023-11-20 14:51:01 +0300

    adding users and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, UsersModule, AuthModule],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AppModule {}

70b3aa9de14ccce806da7d56d94e4d82f4fa4945
Trend20 <enockomondi305@gmail.com>
2023-11-20 15:19:11 +0300

    adding task CRUD operations

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
+
+  @Get('/')
+  getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+
+  @Get()
+  getTask() {
+    console.log('This is the task');
+  }
+
+  // update task by ID
+  updateTak() {
+    console.log('This is the updated task');
+  }
+
+  // delete task by ID
+  deleteTask() {
+    console.log('This is the deleted task');
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = [
+    {
+      name: 'Write documentation',
+      id: 1,
+    },
+    {
+      name: 'Build a house',
+      id: 2,
+    },
+    {
+      name: 'Copy the maximum tables',
+      id: 3,
+    },
+    {
+      name: 'Write frontend tests',
+      id: 4,
+    },
+  ];
+
+  // task operations
+
+  // all tasks
+  getAllTasks() {
+    console.log(this.tasks);
+    return this.tasks;
+  }
+
+  // single task by ID
+  getTask() {
+    console.log(this.tasks);
+    return this.tasks;
+  }
+
+  // update task by ID
+  updateTak() {
+    console.log(this.tasks);
+    return this.tasks;
+  }
+
+  // delete task by ID
+  deleteTask() {
+    console.log(this.tasks);
+    return this.tasks;
+  }
+}

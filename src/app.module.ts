c571d9023ac4edda781ba10afa856d7565a73a96
Trend20 <enockomondi305@gmail.com>
2023-11-22 11:24:23 +0300

    adding the controller readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { UsersModule } from './users/users.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule, UsersModule, AuthModule],
+  imports: [
+    TasksModule,
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,8 +1,10 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateTaskDto {
   @IsNotEmpty()
+  @IsString()
   title: string;
 
   @IsNotEmpty()
+  @IsString()
   description: string;
 }
